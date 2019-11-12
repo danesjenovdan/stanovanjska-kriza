c8de4264a19c4c0079b375bb66644b744f5de731
Filip Muki Dobranic <filipdobranic@gmail.com>
2019-11-12 20:27:41 +0100

    add gimmmick

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="container">
+    <div id="intro-overlay" :class="{hidden: shouldBeHidden}">
+      <h1>{{ question.q }}</h1>
+      <button
+        class="intro-button"
+        ref="theButton"
+        @mouseenter="moveTheButton"
+        @touchstart="moveTheButton"
+      >{{ question.f }}</button>
+      <button
+        class="intro-button"
+        @click="shouldBeHidden = true"
+      >{{ question.a }}</button>
+    </div>
     <Main></Main>
   </div>
 </template>
@@ -25,12 +38,57 @@
     name: 'app',
     components: {
       Main
-    }
+    },
+    data: () => {
+      return {
+        shouldBeHidden: false,
+        questions: [
+          {
+            q: 'Se ti maje streha nad glavo?',
+            a: 'Ja',
+            f: 'Ne',
+          },
+          {
+            q: 'Se vaši otroci lahko preselijo na svoje?',
+            a: 'Ne',
+            f: 'Ja',
+          },
+          {
+            q: 'Si lahko privoščiš prvo stanovanje?',
+            a: 'Ne',
+            f: 'Ja',
+          },
+          {
+            q: 'Tudi tvoja plača raste počasneje kot najemnina?',
+            a: 'Ja',
+            f: 'Ne',
+          },
+          {
+            q: 'Še vedno čakaš na posteljo v študentskem domu?',
+            a: 'Ja',
+            f: 'Ne',
+          },
+        ],
+      }
+    },
+    computed: {
+      question() {
+        return this.questions[Math.floor(Math.random() * this.questions.length)];
+      },
+    },
+    methods: {
+      moveTheButton() {
+        const randX = 20 + Math.floor(Math.random() * (window.innerWidth/3));
+        const randY = 20 + Math.floor(Math.random() * (window.innerHeight/3));
+        this.$refs.theButton.style.top = `${randY}px`;
+        this.$refs.theButton.style.left = `${randX}px`;
+      },
+    },
   }
 
 </script>
 
-<style>
+<style lang="scss">
 
   body {
     margin: 0;
@@ -53,6 +111,42 @@
     height:100vh;
     /*display: flex;*/
     /*padding-bottom: 5px;*/
+  }
+
+  #intro-overlay {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #EFEFF0;
+    color: #62667a;
+    z-index: 100;
+
+    padding: 40px;
+
+    &.hidden {
+      display: none;
+    }
+
+    h1 {
+      font-size: 100px;
+      @media (max-width: 762px) {
+        font-size: 48px;
+      }
+    }
+
+    .intro-button {
+      border-radius: 0;
+      border: 3px solid #62667a;
+      background-color: #EFEFF0;
+      color: #62667a;
+      font-size: 48px;
+      font-family: neue-haas-grotesk-display, sans-serif;
+      position: relative;
+      width: 200px;
+      margin: 20px;
+    }
   }
 
 </style>
